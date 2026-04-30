d942a56d1d365c3efd92989708c95710456102b7
juantabordaalzate <juantabordalzate5@gmail.com>
2026-04-30 15:25:59 -0500

    Ejercicios del 20 al 26 realizadas

---
+++ OperadoresCombinados.java
@@ -0,0 +1,47 @@
+
+public class OperadoresCombinados {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Suma Combinada
+		int a = 50;
+		int b = 50;
+		a+= b = a; // b= b+a
+		
+		System.out.println( "La suma de a=50 y a + b es = " +a);
+		System.out.println("");
+		
+		//Resta Combinada
+		int c = 50;
+		int d = 50;	
+		c-=d = c; // d= d-c
+				
+		System.out.println( "La resta de a=50 y a - b es = " +c);
+		System.out.println("");
+				
+		//Multiplicación Combinada
+		int e = 50;
+		int f = 50;	
+		e*=f = e; // f= f*e
+				
+		System.out.println( "La multiplicación de a=50 y a * b es = " +e);
+		System.out.println("");
+				
+		//División Combinada
+		int g = 50;
+		int h = 50;
+		g/=h = g; // h= h/g
+				
+		System.out.println( "La división de a=50 y a / b es = " +g);
+		System.out.println("");
+				
+		//Módulo Combinado
+		int i = 50;
+		int j = 50;	
+		i=j = i; // j= j % i 
+				
+				System.out.println( "El módulo de a=50 y a % b es = " +i +"%");
+
+	}
+
+}
---
+++ OperadoresIncrementales.java
@@ -0,0 +1,42 @@
+
+
+public class OperadoresIncrementales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Incrementa al lado izquierdo
+		
+		int a = 5;
+		int b = ++a;
+	
+		System.out.println("El valor total en a es: " + a);
+		System.out.println ("El valor total en b es:" + b );
+		System.out.println ("");
+		
+		// Incrementa al lado derecho
+		int c = 5;
+		int d = c++;
+		System.out.println("El valor total en c es: " + c);
+		System.out.println ("El valor total en d es:" + d);
+		System.out.println ("");
+						
+		// Disminuye al lado izquierdo
+		
+		int e = 5;
+		int f = --e;
+	
+		System.out.println("El valor total en a es: " + e);
+		System.out.println ("El valor total en b es: " + f  );
+		System.out.println ("");
+		
+		// Disminuye al lado derecho
+		int g = 5;
+		int h = g--;
+		System.out.println("El valor total en c es: " + g);
+		System.out.println ("El valor total en d es: " + h);
+		
+
+	}
+
+}
